b0bd0519f300ca4ecc786758097966bb8a1631f7
vyankateshpotdar <vyankateshpotdar411@gmail.com>
2026-05-23 09:51:28 +0530

    Upgrade to Prisma 7.8.0 with prisma.config and pg adapter

--- src/db/prismaClient.ts
+++ src/db/prismaClient.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-export const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error('DATABASE_URL is required');
+}
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
